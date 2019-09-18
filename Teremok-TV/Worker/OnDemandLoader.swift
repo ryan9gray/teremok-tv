bc1494740ce1ecc5ffa865ddefed1906f864fca7
Eugene Ivanov <ryan9gray@gmail.com>
2019-09-18 16:46:08 +0300

    OnDemand

---
+++ Teremok-TV/Worker/OnDemandLoader.swift
@@ -0,0 +1,29 @@
+//
+//  OnDemandLoader.swift
+//  Teremok-TV
+//
+//  Created by Eugene Ivanov on 18/09/2019.
+//  Copyright © 2019 xmedia. All rights reserved.
+//
+import Foundation
+
+class OnDemandLoader {
+    private lazy var bundleResourceRequest = NSBundleResourceRequest(tags: Set(["IntroducingVideo"]))
+
+    private func loadOnDemandAssets() {
+        bundleResourceRequest.conditionallyBeginAccessingResources { [unowned self] (available) in
+            if available {
+                //self.loadOnboardingAssets()
+            } else {
+                self.bundleResourceRequest.beginAccessingResources { (error) in
+                    guard error == nil else { return }
+                    //self.loadOnboardingAssets()
+                }
+            }
+        }
+    }
+
+    private func discardOnDemandAssets() {
+        bundleResourceRequest.endAccessingResources()
+    }
+}
