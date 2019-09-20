670254f84eee2d092bf48075fa735d2530cdada1
Eugene Ivanov <ryan9gray@gmail.com>
2019-09-20 18:58:45 +0300

    elsr

--- Teremok-TV/Scene/MonsterGame/MonsterFlow.swift
+++ Teremok-TV/Scene/MonsterGame/MonsterFlow.swift
@@ -20,7 +20,7 @@ class MonsterGameFlow {
     var checkIntro: Bool = true
 
     func startFlow(difficulty: Int) {
-        guard !checkIntro, LocalStore.monsterIntroduce {
+        guard !checkIntro, LocalStore.monsterIntroduce else {
             checkIntro = false
             showIntroduce(difficulty: difficulty)
             return
