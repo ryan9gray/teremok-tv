d0f3d39069b445ab4fb5085cf65c06ca2d619beb
ryan9gray <ryan9gray@gmail.com>
2019-11-09 01:32:05 +0300

    Forground download

--- Teremok-TV/Services/BackgroundSession.swift
+++ Teremok-TV/Services/BackgroundSession.swift
@@ -12,81 +12,32 @@ import UIKit
 import Alamofire
 import UserNotifications
 
-fileprivate let backgroundIdentifier = "ru.xmedia.teremoktv"
+fileprivate let backgroundIdentifier = "ru.xmedia.teremoktv.background"
 fileprivate let notificationIdentifier = "teremoktv"
     
-final class BackgroundSession {
+final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
     static let shared = BackgroundSession()
-    private let manager: SessionManager
 
-    /// Save background completion handler, supplied by app delegate
-    
-    func saveBackgroundCompletionHandler(_ backgroundCompletionHandler: @escaping () -> Void) {
-        manager.backgroundCompletionHandler = backgroundCompletionHandler
-    }
-
-    private init() {
-        let configuration = URLSessionConfiguration.background(withIdentifier: backgroundIdentifier)
-        manager = SessionManager(configuration: configuration)
-        
+    private override init() {
+        super.init()
         NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .MusicBadge, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .FavBadge, object: nil)
+    }
 
-        manager.delegate.downloadTaskDidFinishDownloadingToURL = { [weak self] _, task, location in
-            guard let self = self else { return }
+    private let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    func localFilePath(for url: URL) -> URL {
+      return documentsPath.appendingPathComponent(url.lastPathComponent)
+    }
+    lazy private var downloadsSession: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+    }()
 
-            do {
-                guard
-                    let video = self.list.filter({ $0.fileName == task.originalRequest!.url!.lastPathComponent }).first
-                else { return }
-
-                var destination = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-                if video.fileName.split(separator: ".").last == "m4a" {
-                    self.createMusicFolder()
-                    destination.appendPathComponent("Music")
-                }
-
-                destination.appendPathComponent(video.id)
-                try FileManager.default.copyItem(at: location, to: destination)
-            } catch {
-                print("\(error)")
-            }
-        }
-        
-        /// specify what to do when background session finishes; i.e. make sure to call saved completion handler
-        /// if you don't implement this, it will call the saved `backgroundCompletionHandler` for you
-        manager.delegate.sessionDidFinishEventsForBackgroundURLSession = { [weak self] _ in
-            self?.manager.backgroundCompletionHandler?()
-            self?.manager.backgroundCompletionHandler = nil
-            
-            // if you want, tell the user that the downloads are done
-
-            let content = UNMutableNotificationContent()
-            content.title = "Скачено"
-            content.body = "Ура!"
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-            let notification = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
-            UNUserNotificationCenter.current().add(notification)            
-        }
-
-        manager.delegate.taskDidComplete = { _, task, error in
-            guard let filename = task.originalRequest?.url?.lastPathComponent else { return }
-            if let error = error {
-                print("\(filename) error: \(error)")
-            } else {
-                print("\(filename) done!")
-
-            }
-            if filename.split(separator: ".").last == "m4a" {
-                NotificationCenter.default.post(name: .MusicBadge, object: filename, userInfo: nil)
-            } else {
-                NotificationCenter.default.post(name: .FavBadge, object: filename, userInfo: ["Fav": 1])
-            }
-            NotificationCenter.default.post(name: .UploadProgress, object: 1.0)
-
-            /// I might want to post some event to `NotificationCenter`
-            /// so app UI can be updated, if it's in foreground
-        }
+    private func download(_ video: Video) {
+        downloadAlert = configureAlert(with: "Не выключайте приложение пока идет скачивание")
+        isDownLoad = true
+        let task = downloadsSession.downloadTask(with: video.url)
+        task.resume()
     }
 
     @objc func done(_ notification: Notification) {
@@ -99,14 +50,12 @@ final class BackgroundSession {
     }
 
     var list: Set<Video> = Set()
-
+    var isDownLoad = false
     struct Video: Hashable {
         let url: URL
         let fileName: String
         let id: String
     }
-
-    var isDownLoad = false
 
     private func removeFromList(_ video: Video) {
         self.list.remove(video)
@@ -119,16 +68,7 @@ final class BackgroundSession {
     private func addToList(_ video: Video) {
         list.insert(video)
         if !isDownLoad {
-            self.download(video)
-        }
-    }
-
-    private func download(_ video: Video) {
-        self.isDownLoad = true
-        let utilityQueue = DispatchQueue.global(qos: .utility)
-        manager.download(video.url).downloadProgress(queue: utilityQueue) { progress in
-            NotificationCenter.default.post(name: .UploadProgress, object: progress.fractionCompleted)
-            print("Download Progress: \(progress.fractionCompleted)")
+            download(video)
         }
     }
 
@@ -139,15 +79,113 @@ final class BackgroundSession {
     }
 
     private func createMusicFolder() {
-        let fileManager = FileManager.default
-        let documentsURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let path = documentsURL.appendingPathComponent("Music")
-
+        let path = documentsPath.appendingPathComponent("Music")
         do {
             try FileManager.default.createDirectory(atPath: path.path, withIntermediateDirectories: false, attributes: nil)
         } catch let error as NSError {
             print(error.localizedDescription);
         }
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        guard
+            let sourceURL = downloadTask.originalRequest?.url,
+            let video = self.list.filter({ $0.fileName == sourceURL.lastPathComponent }).first
+        else { return }
+
+        var destination = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+        if video.fileName.split(separator: ".").last == "m4a" {
+           createMusicFolder()
+           destination.appendPathComponent("Music")
+        }
+        destination.appendPathComponent(video.id)
+
+        print(destination)
+        let fileManager = FileManager.default
+        try? fileManager.removeItem(at: destination)
+        do {
+            try fileManager.moveItem(at: location, to: destination)
+            isDownLoad = false
+        } catch let error {
+          print("Could not copy file to disk: \(error.localizedDescription)")
+        }
+
+        DispatchQueue.main.async {
+            self.downloadAlert?.dismiss(animated: true) {
+                let filename = sourceURL.lastPathComponent
+                if filename.split(separator: ".").last == "m4a" {
+                    NotificationCenter.default.post(name: .MusicBadge, object: filename, userInfo: nil)
+                } else {
+                    NotificationCenter.default.post(name: .FavBadge, object: filename, userInfo: ["Fav": 1])
+                }
+                NotificationCenter.default.post(name: .UploadProgress, object: 1.0)
+            }
+        }
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64, totalBytesWritten: Int64,
+                    totalBytesExpectedToWrite: Int64) {
+        //guard let url = downloadTask.originalRequest?.url else { return }
+        //let totalSize = ByteCountFormatter.string(fromByteCount: totalBytesExpectedToWrite, countStyle: .file)
+
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        NotificationCenter.default.post(name: .UploadProgress, object: progress)
+        if progress == 1 {
+
+        }
+        print("\(progress)")
+    }
+
+    class func load(url: URL, to localUrl: URL, completion: @escaping () -> ()) {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        let request = try! URLRequest(url: url, method: .get)
+
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Success: \(statusCode)")
+                }
+
+                do {
+                    try FileManager.default.moveItem(at: tempLocalUrl, to: localUrl)
+                    completion()
+                } catch (let writeError) {
+                    print("error writing file \(localUrl) : \(writeError)")
+                }
+
+            } else {
+                print("Failure: %@", error?.localizedDescription ?? "");
+            }
+        }
+        task.resume()
+    }
+
+    private weak var downloadAlert: UIAlertController?
+    private func configureAlert(with title: String, message: String = "") -> UIAlertController {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Закрыть", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        alertController.applyStyle()
+        UIApplication.topViewController()?.present(alertController, animated: true, completion: nil)
+        return alertController
+    }
+}
+
+extension BackgroundSession: URLSessionDelegate {
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+      DispatchQueue.main.async {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+          let completionHandler = appDelegate.backgroundSessionCompletionHandler {
+          appDelegate.backgroundSessionCompletionHandler = nil
+          completionHandler()
+        }
+      }
     }
 }
 
