e4406bb4359b8e1be1cfae8282b1118438179dbc
ryan9gray <ryan9gray@gmail.com>
2019-11-09 15:11:32 +0300

    BAckground download

--- Teremok-TV/Services/BackgroundSession.swift
+++ Teremok-TV/Services/BackgroundSession.swift
@@ -18,37 +18,6 @@ fileprivate let notificationIdentifier = "teremoktv"
 final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
     static let shared = BackgroundSession()
 
-    private override init() {
-        super.init()
-        NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .MusicBadge, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .FavBadge, object: nil)
-    }
-
-    private let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-    func localFilePath(for url: URL) -> URL {
-      return documentsPath.appendingPathComponent(url.lastPathComponent)
-    }
-    lazy private var downloadsSession: URLSession = {
-        let configuration = URLSessionConfiguration.default
-        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-    }()
-
-    private func download(_ video: Video) {
-        downloadAlert = configureAlert(with: "Не выключайте приложение пока идет скачивание")
-        isDownLoad = true
-        let task = downloadsSession.downloadTask(with: video.url)
-        task.resume()
-    }
-
-    @objc func done(_ notification: Notification) {
-        if let filename = notification.object as? String {
-            print("\(filename) Notify!")
-            if let video = self.list.lazy.filter({ $0.fileName == filename }).first {
-                self.removeFromList(video)
-            }
-        }
-    }
-
     var list: Set<Video> = Set()
     var isDownLoad = false
     struct Video: Hashable {
@@ -57,8 +26,44 @@ final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
         let id: String
     }
 
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .MusicBadge, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.done), name: .FavBadge, object: nil)
+    }
+
+    lazy private var downloadsSession: URLSession = {
+        let configuration = URLSessionConfiguration.background(withIdentifier: backgroundIdentifier)
+        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+    }()
+
+    private func download(_ video: Video) {
+        isDownLoad = true
+        let task = downloadsSession.downloadTask(with: video.url)
+        task.resume()
+    }
+
+    private func notify() {
+        let content = UNMutableNotificationContent()
+        content.title = "Скачено"
+        content.body = "Ура!"
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let notification = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(notification)
+    }
+
+    @objc func done(_ notification: Notification) {
+        if let filename = notification.object as? String {
+            print("\(filename) Notify!")
+            if let video = self.list.lazy.filter({ $0.fileName == filename }).first {
+                notify()
+                self.removeFromList(video)
+            }
+        }
+    }
+
     private func removeFromList(_ video: Video) {
-        self.list.remove(video)
+        list.remove(video)
         isDownLoad = false
         guard let next = list.first else { return }
 
@@ -67,9 +72,9 @@ final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
 
     private func addToList(_ video: Video) {
         list.insert(video)
-        if !isDownLoad {
-            download(video)
-        }
+        guard !isDownLoad else { return }
+
+        download(video)
     }
 
     func download(_ url: URL, name: String) {
@@ -79,6 +84,7 @@ final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
     }
 
     private func createMusicFolder() {
+        guard let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
         let path = documentsPath.appendingPathComponent("Music")
         do {
             try FileManager.default.createDirectory(atPath: path.path, withIntermediateDirectories: false, attributes: nil)
@@ -112,68 +118,22 @@ final class BackgroundSession: NSObject, URLSessionDownloadDelegate {
         }
 
         DispatchQueue.main.async {
-            self.downloadAlert?.dismiss(animated: true) {
-                let filename = sourceURL.lastPathComponent
-                if filename.split(separator: ".").last == "m4a" {
-                    NotificationCenter.default.post(name: .MusicBadge, object: filename, userInfo: nil)
-                } else {
-                    NotificationCenter.default.post(name: .FavBadge, object: filename, userInfo: ["Fav": 1])
-                }
-                NotificationCenter.default.post(name: .UploadProgress, object: 1.0)
+            let filename = sourceURL.lastPathComponent
+            if filename.split(separator: ".").last == "m4a" {
+                NotificationCenter.default.post(name: .MusicBadge, object: filename, userInfo: nil)
+            } else {
+                NotificationCenter.default.post(name: .FavBadge, object: filename, userInfo: ["Fav": 1])
             }
+            NotificationCenter.default.post(name: .UploadProgress, object: 1.0)
         }
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask,
                     didWriteData bytesWritten: Int64, totalBytesWritten: Int64,
                     totalBytesExpectedToWrite: Int64) {
-        //guard let url = downloadTask.originalRequest?.url else { return }
-        //let totalSize = ByteCountFormatter.string(fromByteCount: totalBytesExpectedToWrite, countStyle: .file)
-
         let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
         NotificationCenter.default.post(name: .UploadProgress, object: progress)
-        if progress == 1 {
-
-        }
         print("\(progress)")
-    }
-
-    class func load(url: URL, to localUrl: URL, completion: @escaping () -> ()) {
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig)
-        let request = try! URLRequest(url: url, method: .get)
-
-        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
-            if let tempLocalUrl = tempLocalUrl, error == nil {
-                // Success
-                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                    print("Success: \(statusCode)")
-                }
-
-                do {
-                    try FileManager.default.moveItem(at: tempLocalUrl, to: localUrl)
-                    completion()
-                } catch (let writeError) {
-                    print("error writing file \(localUrl) : \(writeError)")
-                }
-
-            } else {
-                print("Failure: %@", error?.localizedDescription ?? "");
-            }
-        }
-        task.resume()
-    }
-
-    private weak var downloadAlert: UIAlertController?
-    private func configureAlert(with title: String, message: String = "") -> UIAlertController {
-        let alertController = UIAlertController(title: title,
-                                                message: message,
-                                                preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Закрыть", style: .default, handler: nil)
-        alertController.addAction(okAction)
-        alertController.applyStyle()
-        UIApplication.topViewController()?.present(alertController, animated: true, completion: nil)
-        return alertController
     }
 }
 
