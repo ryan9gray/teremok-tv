1445661163371d8d40483e0fa90db9eddeab7603
Виктор Пузаков <>
2020-04-06 14:23:49 +0500

    dinoGame: change home button color

--- Teremok-TV/Scene/DinoGame/Master/DinoMasterViewController.swift
+++ Teremok-TV/Scene/DinoGame/Master/DinoMasterViewController.swift
@@ -63,7 +63,8 @@ class DinoMasterViewController: GameMasterViewController, DinoMasterDisplayLogic
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        homeBtn.gradientColors = Style.Gradients.DinoGame.green.value
+        
         navigationSubscription = router?.subscribeForNavigation { [weak self] canPop in
             self?.homeBtn.setImage(canPop ? UIImage(named: "icBackShadow") : UIImage(named: "icHomeShadow"), for: .normal)
         }
