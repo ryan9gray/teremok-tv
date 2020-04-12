7fa8ccbda6d17a0e9df5c90a0b33687d0b40e6d1
Виктор Пузаков <>
2020-04-12 11:48:47 +0500

    add new cells

---
+++ Teremok-TV/Scene/Main/ExtendedMainCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  ExtendedMainCollectionViewCell.swift
+//  Teremok-TV
+//
+//  Created by Виктор Пузаков on 10.04.2020.
+//  Copyright © 2020 xmedia. All rights reserved.
+//
+
+import UIKit
+
+protocol DidSelectRazdelAt {
+    func goToRazdel(razdel: Int)
+}
+
+class ExtendedMainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private var collectionView: UICollectionView!
+    
+    var serials: [RazdelVCModel.SerialItem] = []
+    
+    //TO DO: отрефакторить это
+    var razdelNumber: Int = 0
+    var delegate: DidSelectRazdelAt?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let cells = [RazdelCollectionViewCell.self, MoreSerialsCollectionViewCell.self, LoadingCollectionViewCell.self]
+        collectionView.register(cells: cells)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension ExtendedMainCollectionViewCell: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            //delegate?.gotoSerial(razdelID: indexPath.row, razdelTitle: "")
+        } else {
+            delegate?.goToRazdel(razdel: razdelNumber)
+        }
+    }
+}
+
+extension ExtendedMainCollectionViewCell: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        } else {
+            return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 0 {
+            let cell = collectionView.dequeueReusableCell(withCell: RazdelCollectionViewCell.self, for: indexPath)
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withCell: MoreSerialsCollectionViewCell.self, for: indexPath)
+            return cell
+        }
+    }
+}
